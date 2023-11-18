2703fde822d737645b3e53056854ad22c8beddf3
Luis <luisgservsp@gmail.com>
2023-11-18 22:14:19 +0000

    Second commit

---
+++ src/main/java/dacd/guillen/model/Location.java
@@ -0,0 +1,24 @@
+package dacd.guillen.model;
+
+public class Location {
+    private double lat;
+    private double lon;
+    private String name;
+
+    public Location(double lat, double lon, String name) {
+        this.lat = lat;
+        this.lon = lon;
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/dacd/guillen/model/WeatherData.java
@@ -0,0 +1,60 @@
+package dacd.guillen.model;
+import java.time.Instant;
+
+public class WeatherData {
+    private double temp;
+    private double rain;
+    private double humidity;
+    private double cloudiness;
+    private double windSpeed;
+    private Location location;
+    private Instant instant;
+    private String weatherDescription;
+    private String weatherIcon;
+
+    public WeatherData(double temp, double rainProbability, double humidity, double cloudiness, double windSpeed, Location location, Instant instant, String weatherDescription, String weatherIcon) {
+        this.temp = temp;
+        this.rain = rainProbability;
+        this.humidity = humidity;
+        this.cloudiness = cloudiness;
+        this.windSpeed = windSpeed;
+        this.location = location;
+        this.instant = instant;
+        this.weatherDescription = weatherDescription;
+        this.weatherIcon = weatherIcon;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public double getRainProbability() {
+        return rain;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getCloudiness() {
+        return cloudiness;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+    public String getWeatherIcon() {
+        return weatherIcon;
+    }
+}
